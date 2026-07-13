9593fef33e3a949043ac35170d7a99c41fdb3f6a
nveshaan <nveshaan@gmail.com>
2026-07-13 17:36:07 +0530

    add evoformer/evoformer

---
+++ src/evoformer/evoformer.py
@@ -0,0 +1,90 @@
+import torch.nn as nn
+from evoformer.dropout import DropoutRowwise
+from evoformer.msa_stack import MSARowAttentionWithPairBias, MSAColumnAttention, OuterProductMean, MSATransition
+from evoformer.pair_stack import PairStack
+from torch import Tensor
+
+
+class EvoformerBlock(nn.Module):
+    """
+    Implements one block from Algorithm 6.
+    """
+    
+    def __init__(self, c_m: int, c_z: int):
+        """Initializes EvoformerBlock.
+
+        Args:
+            c_m (int): Embedding dimension for the MSA representation.
+            c_z (int): Embedding dimension for the pair representation.
+        """
+        super().__init__()
+
+        self.dropout_rowwise_m = DropoutRowwise(p=0.15)
+        self.msa_att_row = MSARowAttentionWithPairBias(c_m, c_z)
+        self.msa_att_col = MSAColumnAttention(c_m)
+        self.msa_transition = MSATransition(c_m)
+        self.outer_product_mean = OuterProductMean(c_m, c_z)
+        self.core = PairStack(c_z)
+
+    def forward(self, m: Tensor, z: Tensor) -> tuple[Tensor, Tensor]:
+        """
+        Implements the forward pass for one block in Algorithm 6.
+
+        Args:
+            m (torch.tensor): MSA representation of shape (*, N_seq, N_res, c_m).
+            z (torch.tensor): Pair representation of shape (*, N_res, N_res, c_z).
+
+        Returns:
+            tuple: Transformed tensors m and z of the same shape as the inputs.
+        """
+
+        m = m + self.dropout_rowwise_m(self.msa_att_row(m, z))
+        m = m + self.msa_att_col(m)
+        m = m + self.msa_transition(m)
+
+        z = z + self.outer_product_mean(m)
+        z = self.core(z)
+
+        return m, z
+
+
+class EvoformerStack(nn.Module):
+    """
+    Implements Algorithm 6.
+    """
+    
+    def __init__(self, c_m: int, c_z: int, num_blocks: int, c_s: int=384):
+        """
+        Initializes the EvoformerStack.
+
+        Args:
+            c_m (int): Embedding dimension of the MSA representation.
+            c_z (int): Embedding dimension of the pair representation.
+            num_blocks (int): Number of blocks for the Evoformer.
+            c_s (int, optional): Number of channels for the single representation. 
+                Defaults to 384.
+        """
+        super().__init__()
+
+        self.blocks = nn.ModuleList([EvoformerBlock(c_m, c_z) for _ in range(num_blocks)])
+        self.linear = nn.Linear(c_m, c_z)
+
+    def forward(self, m: int, z: int) -> tuple[Tensor, Tensor, Tensor]:
+        """
+        Implements the forward pass for Algorithm 6.
+
+        Args:
+            m (torch.tensor): MSA representation of shape (*, N_seq, N_res, c_m).
+            z (torch.tensor): Pair representation of shape (*, N_res, N_res, c_z).
+
+        Returns:
+            tuple: Output tensors m, z, and s, where m and z have the same shape
+                as the inputs and s has shape (*, N_res, c_s)  
+        """
+        
+        for evo_block in self.blocks:
+            m, z = evo_block(m, z)
+
+        s = self.linear(m[..., 0, :, :])
+        
+        return m, z, s
