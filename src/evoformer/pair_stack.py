1087b8e21d7853818f0a8d975f0f9bbf3ad49baf
nveshaan <nveshaan@gmail.com>
2026-07-13 17:25:37 +0530

    add evoformer/pair_stack

---
+++ src/evoformer/pair_stack.py
@@ -0,0 +1,198 @@
+import torch
+import torch.nn as nn
+from evoformer.dropout import DropoutRowwise, DropoutColumnwise
+from attention.mha import MultiHeadAttention
+from torch import Tensor
+
+
+class TriangleMultiplication(nn.Module):
+    """
+    Implements Algorithm 11 and Algorithm 12.
+    """
+    def __init__(self, c_z: int, mult_type: str, c: int=128):
+        """Initialization of TriangleMultiplication.
+
+        Args:
+            c_z (int): Embedding dimension of the pair representation.
+            mult_type (str): Either 'outgoing' for Algorithm 11 or 'incoming' for Algorithm 12.
+            c (int, optional): Embedding dimension . Defaults to 128.
+
+        Raises:
+            ValueError: _description_
+        """
+        super().__init__()
+        if mult_type not in {'outgoing', 'incoming'}:
+            raise ValueError(f'mult_type must be either "outgoing" or "incoming" but is {mult_type}.')
+
+        self.mult_type = mult_type
+
+        self.layer_norm_in = nn.LayerNorm(c_z)
+        self.layer_norm_out = nn.LayerNorm(c)
+        self.linear_a_p = nn.Linear(c_z, c)
+        self.linear_a_g = nn.Linear(c_z, c)
+        self.linear_b_p = nn.Linear(c_z, c)
+        self.linear_b_g = nn.Linear(c_z, c)
+        self.linear_g = nn.Linear(c_z, c_z)
+        self.linear_z = nn.Linear(c, c_z)
+
+    def forward(self, z: Tensor) -> Tensor:
+        """
+        Implements the forward pass for Algorithm 11 or Algorithm 12, depending
+        on if mult_type is set to 'outgoing' or 'incoming'.
+
+        Args:
+            z (torch.tensor): Pair representation of shape (*, N_res, N_res, c_z).
+
+        Returns:
+            torch.tensor: Output tensor of the  same shape as z.
+        """
+
+        z = self.layer_norm_in(z)
+        a = torch.sigmoid(self.linear_a_g(z)) * self.linear_a_p(z)
+        b = torch.sigmoid(self.linear_b_g(z)) * self.linear_b_p(z)
+        g = torch.sigmoid(self.linear_g(z))
+
+        if self.mult_type is "outgoing":
+            z = torch.einsum('...ikc,...jkc->ijc', a, b)
+        else:
+            z = torch.einsum('...kic,...kjc->ijc', a, b)
+
+        out = g * self.linear_z(self.layer_norm_out(z))
+        return out
+
+
+class TriangleAttention(nn.Module):
+    """
+    Implements Algorithm 13 and Algorithm 14.
+    """
+    def __init__(self, c_z: int, node_type: str, c: int=32, N_head: int=4):
+        """
+        Initialization of TriangleAttention.
+
+        Args:
+            c_z (int): Embedding dimension of the pair representation.
+            node_type (str): Either 'starting_node' for Algorithm 13 or 'ending_node' for Algorithm 14.
+            c (int, optional): Embedding dimension for multi-head attention. Defaults to 32.
+            N_head (int, optional): Number of heads for multi-head attention. Defaults to 4.
+
+        Raises:
+            ValueError: _description_
+        """
+        super().__init__()
+        if node_type not in {'starting_node', 'ending_node'}:
+            raise ValueError(f'node_type must be either "starting_node" or "ending_node" but is {node_type}')
+
+        self.node_type = node_type
+
+        self.layer_norm = nn.LayerNorm(c_z)
+        if node_type is 'starting_node':
+            attn_dim = -2
+        else:
+            attn_dim = -3
+
+        self.mha = MultiHeadAttention(c_z, c, N_head, attn_dim, gated=True)
+        self.linear = nn.Linear(c_z, N_head, bias=False)
+
+    def forward(self, z: Tensor) -> Tensor:
+        """
+        Implements the forward pass for Algorithm 13 or Algorithm 14, depending
+        if node_type is set to 'starting_node' or 'ending_node'.
+
+        Args:
+            z (torch.tensor): Pair representation of shape (*, N_res, N_res, c_z).
+
+        Returns:
+            torch.tensor: Output tensor of the same shape as z.
+        """
+
+        z = self.layer_norm(z)
+        b = self.linear(z)
+        b = b.moveaxis(-1, -3)
+
+        if self.node_type is 'ending_node':
+            b = b.transpose(-1, -2)
+
+        out = self.mha(z, bias=b)
+
+        return out
+
+
+class PairTransition(nn.Module):
+    """
+    Implements Algorithm 15.
+    """
+    
+    def __init__(self, c_z: int, n: int=4):
+        """
+        Initializes the PairTransition.
+
+        Args:
+            c_z (int): Embedding dimension of the pair representation.
+            n (int, optional): Factor by which the number of intermediate channels 
+                expands the original number of channels. 
+                Defaults to 4.
+        """
+        super().__init__()
+
+        self.layer_norm = nn.LayerNorm(c_z)
+        self.linear_1 = nn.Linear(c_z, n*c_z)
+        self.relu = nn.ReLU()
+        self.linear_2 = nn.Linear(n*c_z, c_z)
+
+    def forward(self, z: Tensor) -> Tensor:
+        """
+        Forward pass for Algorithm 15.
+
+        Args:
+            z (torch.tensor): Pair representation of shape (*, N_res, N_res, c_z).
+
+        Returns:
+            torch.tensor: Output tensor of the same shape as z.
+        """
+
+        z = self.layer_norm(z)
+        a = self.linear_1(z)
+        out = self.linear_2(self.relu(a))
+
+        return out
+
+
+class PairStack(nn.Module):
+    """
+    Implements the pair stack from Algorithm 6.
+    """
+
+    def __init__(self, c_z: int):
+        """
+        Initializes the PairStack.
+
+        Args:
+            c_z (int): Embedding dimension of the pair representation.
+        """
+        super().__init__()
+
+        self.dropout_rowwise = DropoutRowwise(p=0.25)
+        self.dropout_columnwise = DropoutColumnwise(p=0.25)
+        self.tri_mul_out = TriangleMultiplication(c_z, mult_type='outgoing')
+        self.tri_mul_in = TriangleMultiplication(c_z, mult_type='incoming')
+        self.tri_att_start = TriangleAttention(c_z, node_type='starting_node')
+        self.tri_att_end = TriangleAttention(c_z, node_type='ending_node')
+        self.pair_transition = PairTransition(c_z)
+
+    def forward(self, z: Tensor) -> Tensor:
+        """
+        Implements the forward pass for the pair stack from Algorithm 6.
+
+        Args:
+            z (torch.tensor): Pair representation of shape (*, N_res, N_res, c_z).
+
+        Returns:
+            torch.tensor: Output tensor of the same shape as z.
+        """
+
+        z = z + self.dropout_rowwise(self.tri_mul_out(z))
+        z = z + self.dropout_rowwise(self.tri_mul_in(z))
+        z = z + self.dropout_rowwise(self.tri_att_start(z))
+        z = z + self.dropout_columnwise(self.tri_att_end(z))
+
+        return z
